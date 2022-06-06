e05b50c907eab2863ddbe83a668e328ee1daef7b
lautarovilas <vilaslautaro@gmail.com>
2022-06-06 19:15:40 -0300

    easily structure the arrays of course, technology and project components, so that the customer can easily add or remove new elements in them.

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -10,45 +10,7 @@ import {
 } from "./courses.styles";
 import { motion } from "framer-motion";
 import useWidth from "hooks/useWidth";
-
-const courses = [
-  {
-    id: 1,
-    name: "Vuejs 2 - the complete guide",
-    text: "One the most popular Javascript Framework (Maximilian Schawarzmuller)",
-    academy: "Udemy",
-  },
-  {
-    id: 2,
-    name: "The complete Javascript course",
-    text: "Proyects, challenges, Javascript ES6+, OOP, AJAX, Webpack (Jonas Schmedtmann)",
-    academy: "Udemy",
-  },
-  {
-    id: 3,
-    name: "Frontend Vue.js Carrer",
-    text: "HTML, CSS, SASS, SEO, Javascript, Vue.js",
-    academy: "Coderhouse",
-  },
-  {
-    id: 4,
-    name: "Desarrollo web",
-    text: "HTML5, CSS3, JavaScript, jQuery, AJAX, PHP, MySQL (Juan de la Torre)",
-    academy: "Udemy",
-  },
-  {
-    id: 5,
-    name: "Learn Scrum",
-    text: "Linkedin",
-    academy: "Scrum Basics",
-  },
-  {
-    id: 6,
-    name: "Git Started with GitHub",
-    text: "Save project changes into source control using Git, Create repositories on GitHub. Pull request, and branchs use. (Jason Tylor)",
-    academy: "Udemy",
-  },
-];
+import { courses } from "arrays/arrayCourses";
 
 export const Courses = () => {
   const {width} = useWidth()
--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -6,49 +6,7 @@ import {
 } from "./technologies.styles";
 import { motion } from "framer-motion";
 import useWidth from "hooks/useWidth";
-
-const technologies = [
-  {
-    id: 1,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197835/portfolio%20daniel/Vector-1_obwlvo.png",
-    text: "html5",
-  },
-  {
-    id: 2,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197835/portfolio%20daniel/Vector_r9q079.png",
-    text: "CSS3",
-  },
-  {
-    id: 3,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197835/portfolio%20daniel/Vector-3_gwkauk.png",
-    text: "JAVASCRIPT",
-  },
-  {
-    id: 4,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197835/portfolio%20daniel/Vector-2_faxixr.png",
-    text: "SASS",
-  },
-  {
-    id: 5,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197835/portfolio%20daniel/Vector-4_j80kk7.png",
-    text: "VUEJS",
-  },
-  {
-    id: 6,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1653403406/portfolio%20daniel/logoB_hiikom.svg",
-    text: "BOOTSTRAP",
-  },
-  {
-    id: 7,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197836/portfolio%20daniel/Vector-5_bgz8l6.png",
-    text: "DOCKER",
-  },
-  {
-    id: 8,
-    img: "https://res.cloudinary.com/dn7qsxzdf/image/upload/v1652197836/portfolio%20daniel/Vector-6_yessvx.png",
-    text: "NODEJS",
-  },
-];
+import { technologies } from "arrays/arrayTechnologies";
 
 export const Technologies = () => {
   const { refTechnologies } = useRefs();
